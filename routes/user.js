97d7a4d80f9c875005fb40d7e659120b26de414e
vkanadecv <evgeny@purpleadlab.com>
2021-11-05 17:32:10 +0200

    Revert "day3"
    This reverts commit c54b7255039bce1a59f7d9a4732d5b3f59f10220.

--- routes/user.js
+++ routes/user.js
@@ -3,16 +3,12 @@ exports.signup = function(req, res){
    if(req.method == "POST"){
       var post  = req.body;
       var username = post.username;
-      var email = post.email;
-      var phone = post.phone;
-      var country = post.country;
-      var city = post.city;
-      var address = post.address;
       var password = post.password;
-      var confpassword = post.confpassword;
-     console.log(post);
-	  if(username !='' && password!='' && password==confpassword) {
-		  var sql = "INSERT INTO users(name,email,phone,country,city,address,password) VALUES ('" + username + "','" + email + "','" + phone + "','" + country + "','" + city + "','" + address + "','" + password + "')";
+      var fname= post.first_name;
+      var lname= post.last_name;
+      var mobile= post.mobile;
+	  if(username !='' && password!='') {
+		  var sql = "INSERT INTO users(first_name,last_name,mobile,username, password) VALUES ('" + fname + "','" + lname + "','" + mobile + "','" + username + "','" + password + "')";
 
 		  var query = db.query(sql, function(err, result) {
 			 message = "Your account has been created succesfully2.";
@@ -35,10 +31,10 @@ exports.login = function(req, res){
 
    if(req.method == "POST"){
       var post  = req.body;
-      var email = post.email;
+      var username = post.username;
       var password= post.password;
      
-      var sql="SELECT id, name, email, phone, country, city, address FROM `users` WHERE `email`='"+email+"' and password = '"+password+"'";                           
+      var sql="SELECT id, first_name, last_name, username FROM `users` WHERE `username`='"+username+"' and password = '"+password+"'";                           
       db.query(sql, function(err, results){       
          if(results.length){
             req.session.userId = results[0].id;
@@ -76,32 +72,22 @@ exports.dashboard = function(req, res, next){
    });       
 };
 
-exports.dialin = function(req, res){
-           
-   message = '';
-   if(req.method == "POST"){
-      var post  = req.body;
-      var deal_title = post.deal_title;
-      var company_name = post.company_name;
-      var client_name = post.client_name;
-      var client_email = post.client_email;
-      var client_phone = post.client_phone;
-      var offer_cost = post.offer_cost;
-     console.log(post);
-     if(deal_title !='') {
-        var sql = "INSERT INTO deals(deal_title,company_name,client_name,client_email,client_phone,offer_cost) VALUES ('" + deal_title + "','" + company_name + "','" + client_name + "','" + client_email + "','" + client_phone + "','" + offer_cost + "')";
+exports.profile = function(req, res){
 
-        var query = db.query(sql, function(err, result) {
-          message = "Your account has been created succesfully2.";
-          res.render('dashboard.ejs',{message: message});
-         // console.log(post);
-        });
-     } else {
-        message = "Username and password is mandatory field.";
-        res.render('dashboard.ejs',{message: message});
-     }
-
-   } else {
-      res.render('dashboard.ejs');
+   var userId = req.session.userId;
+   if(userId == null){
+      res.redirect("/login");
+      return;
    }
+
+   var sql="SELECT * FROM `users` WHERE `id`='"+userId+"'";          
+   db.query(sql, function(err, result){  
+      res.render('profile.ejs',{data:result});
+   });
+};
+
+exports.logout=function(req,res){
+   req.session.destroy(function(err) {
+      res.redirect("/login");
+   })
 };
